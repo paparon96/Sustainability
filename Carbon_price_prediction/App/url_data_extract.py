65b73857e57ccee11f4dc4489b5886aa76703811
paparon96 <>
2022-08-24 23:00:28 +0200

    Add article text cleaning from raw HTML

--- Carbon_price_prediction/App/url_data_extract.py
+++ Carbon_price_prediction/App/url_data_extract.py
@@ -1,13 +1,21 @@
 import aiohttp
 import asyncio
+import requests
 
 import pandas as pd
+
+from goose3 import Goose
+
+extractor = Goose()
 
 async def fetch(session, url):
     async with session.get(url) as response:
         if response.status != 200:
             response.raise_for_status()
+        # article = extractor.extract(raw_html=response.text())
+        # return await article.cleaned_text
         return await response.text()
+        # return await response.content
 
 async def fetch_all(session, urls):
     tasks = []
@@ -22,7 +30,20 @@ async def main():
             'https://www.washingtonexaminer.com/policy/defense-national-security/us-denies-sanctions-food-crisis-russia-africa-west']
     async with aiohttp.ClientSession() as session:
         htmls = await fetch_all(session, urls)
-        print(pd.DataFrame({'url': urls, 'html': htmls}))
+        df = pd.DataFrame({'url': urls, 'html': htmls})
+        df['text'] = df['html'].apply(lambda x: extractor.extract(raw_html=x).cleaned_text)
+        df.to_csv('./data/article_text.csv', index=False)
 
 if __name__ == '__main__':
     asyncio.run(main())
+
+    # Old approach output
+    response = requests.get('https://www.eubusiness.com/topics/commission/top-stories/w22-30', timeout=5)
+    article = extractor.extract(raw_html=response.content)
+    text = article.cleaned_text
+
+    # New approach output
+    df = pd.read_csv('./data/article_text.csv')
+    text2 = str(df['text'].iloc[0])
+
+    assert text == text2

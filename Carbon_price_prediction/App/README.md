fbd8a344b6c2c9b7bcd9027913002dd1b8b1dc18
paparon96 <>
2022-09-06 23:21:55 +0200

    Add keyword matrix generation script

--- Carbon_price_prediction/App/README.md
+++ Carbon_price_prediction/App/README.md
@@ -2,6 +2,7 @@
 1. Run `time python gdelt_data_extract.py` (around 1-2 minutes for 1 year's data)
 2. Run `time python url_data_extract.py` (around 1-2 minutes for 1 year's data)
 3. Run `time python nlp_preprocess.py` (less than 1 minute for 1 year's data)
+4. Run `time python keyword_matrix_generation.py` (less than 1 minute for 1 year's data)
 
 
 # Running the app
---
+++ Carbon_price_prediction/App/keyword_matrix_generation.py
@@ -0,0 +1,46 @@
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+
+
+# Parameters / Constants
+ngrams = range(1, 4)
+
+# Import data
+text_df = pd.read_csv( "./data/lemmatized_merged_articles.csv", index_col=0)
+print(text_df.head())
+
+# Term-document matrix generation
+for ngram in ngrams:
+    print(f"{ngram}-grams")
+
+    # Count Vectorizer
+    vect = CountVectorizer(ngram_range=(ngram, ngram))
+    vects = vect.fit_transform(text_df.lemmatized_text)
+
+    td_matrix_cols = vect.get_feature_names()
+    sparse_matrix = vects
+
+    # Import climate change keyword list
+    carbon_keywords = pd.read_csv( "./data/keyword_lists/revised_keyword_list.csv")
+    carbon_keywords['keywords'] = carbon_keywords['keywords'].\
+                                  apply(lambda x: x.lower())
+
+    # Replace logic for lemmatization
+    carbon_keywords['keywords'] = carbon_keywords['keywords'].\
+    replace({"emissions trading system": "emission trading system",
+             "emissions trading scheme": "emission trading scheme"})
+
+    carbon_keywords_index = {carbon_keyword: td_matrix_cols.index(carbon_keyword)
+                         for carbon_keyword in carbon_keywords.squeeze().values
+                         if carbon_keyword in td_matrix_cols}
+
+    carbon_keyword_matrix = sparse_matrix.tocsr()[:,
+    list(carbon_keywords_index.values())].todense()
+
+    carbon_keyword_df = pd.DataFrame(carbon_keyword_matrix,
+    columns=list(carbon_keywords_index.keys()))
+    print(carbon_keyword_df.head())
+
+    print((carbon_keyword_df > 0).sum(axis=0))
+
+    carbon_keyword_df.to_csv(f'./data/merged_articles_carbon_keyword_term_document_matrix_ngram_{ngram}.csv')

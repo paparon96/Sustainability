3a41e0804c3ecd2964bd4dee4faefa8262e26ccf
paparon96 <>
2022-09-14 22:47:59 +0200

    Add aggregated TF-IDF score time series and extend dashboard/app features

--- Carbon_price_prediction/App/dashboard.py
+++ Carbon_price_prediction/App/dashboard.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from datetime import datetime
 
 # Custom functions
 @st.cache
@@ -26,14 +27,55 @@ control_var_name_map = {'gas_price': 'gas prices',
 
 st.title('EU ETS market dashboard')
 
+st.markdown(
+"""
+# Authors
+- Aron Hartvig
+- Peter Palos
+- Aron Pap
+"""
+)
+st.markdown(
+"""
+# Paper abstract
+etctetctetetcetctectetc
+"""
+)
+
+start_date = st.sidebar.slider(
+    "Analysis start date",
+    value=datetime(2018, 1, 1),
+    format="YY-MM-DD")
+
+end_date = st.sidebar.slider(
+    "Analysis end date",
+    min_value=datetime(2018, 1, 1),
+    value=datetime(2021, 11, 30),
+    max_value=datetime(2021, 12, 12),
+    format="YY-MM-DD")
+
 daily_prices = pd.read_csv( "../Data/merged_dataset.csv", index_col=0,
                          parse_dates=True, dayfirst=True)
+daily_prices = daily_prices[(daily_prices.index >= start_date) &
+                            (daily_prices.index <= end_date)]
 daily_prices.index.name = 'date'
 daily_prices.head()
 
 tf_idf = pd.read_csv(f'../Data/signals/{methodology}_{data_source}_{glossary_source}_{version}keywords.csv',
                      index_col=0, parse_dates=True)
 tf_idf.index.name = 'date'
+
+tf_idf_aggr = pd.read_csv(f'../Data/signals/{methodology}_{data_source}_lemmatized_aggregated_{version}keywords.csv',
+                     index_col=0, parse_dates=True)
+tf_idf_aggr.index.name = 'date'
+tf_idf_aggr = tf_idf_aggr.rename(columns = {'0': 'Aggregated score'})
+
+# Join aggregated TF-IDF score column
+tf_idf = tf_idf.join(tf_idf_aggr)
+
+# Filter TF-IDF dataframe for the relevant date range
+tf_idf = tf_idf.loc[(tf_idf.index >= start_date) & (tf_idf.index <= end_date)]
+
 
 
 st.subheader('Raw data on carbon price and control variables')
@@ -74,3 +116,10 @@ selected_keyword_group = st.sidebar.selectbox('Select keyword group for TF-IDF s
 st.subheader(f'TF-IDF score history for keyword group: {selected_keyword_group} \
 ({rolling_mean_length}-days moving average)')
 st.line_chart(tf_idf[[list(tf_idf.columns)[0], selected_keyword_group]].rolling(rolling_mean_length).mean())
+
+st.markdown(
+"""
+# Sources / References
+- [EU ETS data source](https://ember-climate.org/data/data-tools/carbon-price-viewer/)
+"""
+)
